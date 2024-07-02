c9f62bccf8d3b8bd39a08962fbff307a776e3dfc
unknown <masummim50@gmail.com>
2024-07-02 13:23:01 +0600

    sl

---
+++ api/index.ts
@@ -0,0 +1,37 @@
+import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import http from "http";
+import express, { Application } from "express";
+import cors from "cors";
+
+const app: Application = express();
+app.use(cors());
+app.use(express.json());
+const httpServer = http.createServer(app);
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
+
+const startApolloServer = async (app: Application, httpServer:http.Server) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+  server.applyMiddleware({ app } as any);
+};
+
+startApolloServer(app, httpServer);
+
+export default httpServer;
